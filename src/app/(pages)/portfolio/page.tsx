85acddaeb04bd718a57a3003d139d7b5d1185a15
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-09 14:33:39 -0300

    feat: seção sobre

--- src/app/(pages)/portfolio/page.tsx
+++ src/app/(pages)/portfolio/page.tsx
@@ -1,7 +1,11 @@
+import AboutMe from "@/components/apresentation/AboutMe";
 import Apresentation from "@/components/apresentation/Apresentation";
 
 export default function Portfolio(props: any){
     return (
-        <Apresentation/>
+        <>
+            <Apresentation/>
+            <AboutMe/>
+        </>
     )
 }
---
+++ src/components/apresentation/AboutMe.tsx
@@ -0,0 +1,14 @@
+import { TextGenerateEffect } from "../ui/text-generate-effect";
+
+export default function AboutMe() {
+    return (
+        <section className="mt-6 max-w-[60%]">
+            <h2 className="text-1xl sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-bold bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">Sobre</h2>
+            <TextGenerateEffect
+                words="Adoro desenvolver sistemas web e mobile com interfaces que garantem uma boa usabilidade. No desenvolvimento web, tenho 4 anos de experiência com React.js, 1 ano com Next.js e Angular, já no mobile, estou iniciando minha experiência com React Native. Busco uma oportunidade como desenvolvedor Frontend ou web Developer em uma empresa que trabalhe com tecnologia e ofereça um ótimo ambiente de desenvolvimento, com uma equipe produtiva e unida, ajudando todos os devs em seus problemas para juntos chegarem ao sucesso."
+                className="text-1xl sm:text-1xl md:text-2xl lg:text-3xl tracking-wider font-bold text-white indent-8"
+                duration={0.5}
+            />
+        </section>
+    )
+}
