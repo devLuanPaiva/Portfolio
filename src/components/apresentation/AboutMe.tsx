f8cb7fa44fcbf0b00d2f4017ea6518fa6633f9de
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-12 16:37:00 -0300

    feat: cria component titulo para seções

--- src/components/apresentation/AboutMe.tsx
+++ src/components/apresentation/AboutMe.tsx
@@ -1,11 +1,10 @@
+import TitleSection from "../shared/TitleSection";
 import { TextGenerateEffect } from "../ui";
 
 export default function AboutMe() {
     return (
         <section className="mt-6 max-w-[60%] z-10 relative top-10">
-            <h2 className="text-1xl sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-bold bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">
-                Sobre
-            </h2>
+            <TitleSection>Sobre</TitleSection>
             <TextGenerateEffect
                 words="Adoro desenvolver sistemas web e mobile com interfaces que garantem uma boa usabilidade. No desenvolvimento web, tenho 4 anos de experiência com React.js, 1 ano com Next.js e Angular. Já no mobile, estou iniciando minha experiência com React Native. Busco uma oportunidade como desenvolvedor Frontend ou web Developer em uma empresa que trabalhe com tecnologia e ofereça um ótimo ambiente de desenvolvimento, com uma equipe produtiva e unida, ajudando todos os devs em seus problemas para juntos chegarem ao sucesso."
                 className="text-1xl sm:text-1xl md:text-2xl lg:text-3xl tracking-wider font-bold text-white indent-8"
