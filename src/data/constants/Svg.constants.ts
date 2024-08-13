1d23c80d85a6303d87c350b36bd6b932cb6aa66d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-13 10:02:57 -0300

    feat: cria interface e constantes para icones svgs

---
+++ src/data/constants/Svg.constants.ts
@@ -0,0 +1,133 @@
+import { Svg } from "../interfaces";
+import {
+  AngularIcon,
+  AwsIcon,
+  CssIcon,
+  DjangoIcon,
+  FigmaIcon,
+  GitIcon,
+  GulpIcon,
+  HtmlIcon,
+  JavascriptIcon,
+  MysqlIcon,
+  NestjsIcon,
+  NextjsIcon,
+  NodejsIcon,
+  PostgresqlIcon,
+  PrismaIcon,
+  ReactIcon,
+  SassIcon,
+  ScrumIcon,
+  TailwindIcon,
+  TypescriptIcon,
+  UMLIcon,
+} from "../../../public/svg";
+
+const svg: Svg[] = [
+  {
+    id: "1",
+    path: `${AngularIcon.src}`,
+    name: "Angular",
+  },
+  {
+    id: "2",
+    path: `${AwsIcon.src}`,
+    name: "AWS",
+  },
+  {
+    id: "3",
+    path: `${CssIcon.src}`,
+    name: "CSS3",
+  },
+  {
+    id: "4",
+    path: `${DjangoIcon.src}`,
+    name: "Django",
+  },
+  {
+    id: "5",
+    path: `${FigmaIcon.src}`,
+    name: "Figma",
+  },
+  {
+    id: "6",
+    path: `${GitIcon.src}`,
+    name: "Git",
+  },
+  {
+    id: "7",
+    path: `${GulpIcon.src}`,
+    name: "Gulp",
+  },
+  {
+    id: "8",
+    path: `${HtmlIcon.src}`,
+    name: "HTML5",
+  },
+  {
+    id: "9",
+    path: `${JavascriptIcon.src}`,
+    name: "JavaScript",
+  },
+  {
+    id: "10",
+    path: `${MysqlIcon.src}`,
+    name: "MySQL",
+  },
+  {
+    id: "11",
+    path: `${NestjsIcon.src}`,
+    name: "NestJS",
+  },
+  {
+    id: "12",
+    path: `${NextjsIcon.src}`,
+    name: "NextJS",
+  },
+  {
+    id: "13",
+    path: `${NodejsIcon.src}`,
+    name: "NodeJS",
+  },
+  {
+    id: "14",
+    path: `${PostgresqlIcon.src}`,
+    name: "PostgreSQL",
+  },
+  {
+    id: "15",
+    path: `${PrismaIcon.src}`,
+    name: "Prisma",
+  },
+  {
+    id: "16",
+    path: `${ReactIcon.src}`,
+    name: "ReactJS",
+  },
+  {
+    id: "17",
+    path: `${SassIcon.src}`,
+    name: "SASS",
+  },
+  {
+    id: "18",
+    path: `${ScrumIcon.src}`,
+    name: "Scrum",
+  },
+  {
+    id: "19",
+    path: `${TailwindIcon.src}`,
+    name: "TailwindCSS",
+  },
+  {
+    id: "20",
+    path: `${TypescriptIcon.src}`,
+    name: "TypeScript",
+  },
+  {
+    id: "21",
+    path: `${UMLIcon.src}`,
+    name: "UML",
+  },
+];
+export default svg;
