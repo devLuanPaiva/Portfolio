e074bf9e1fc4ae1564a840368d3cbbb406450426
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-14 16:40:06 -0300

    refactor: envolve a página principal com o 'QueryClientProvider'

--- src/app/(pages)/project/page.tsx
+++ src/app/(pages)/project/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 import WrappedProjectPage from "@/components/projects/WrappedProjectPage";
 import LoadingComponent from "@/components/shared/LoadingComponent";
+import queryClient from "@/data/contexts/queryClient";
 import { Suspense } from "react";
+import { QueryClientProvider } from "react-query";
 
 export default function Project() {
     return (
-        <Suspense fallback={<LoadingComponent />}>
-            <WrappedProjectPage />
-        </Suspense >
+        <QueryClientProvider client={queryClient}>
+            <Suspense fallback={<LoadingComponent />}>
+                <WrappedProjectPage />
+            </Suspense >
+        </QueryClientProvider>
     );
 }
