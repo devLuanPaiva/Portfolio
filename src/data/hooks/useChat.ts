ad7f2ea007895e9b8640d8958143c61b1af1999c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-01-23 09:28:33 -0300

    refactor: correcting the interface nomenclature

--- src/data/hooks/useChat.ts
+++ src/data/hooks/useChat.ts
@@ -1,32 +1,34 @@
 "use client"
 import { useState } from "react"
 import { useSessionStorage } from "./"
-import { Message } from "../models/interfaces"
+import { IMessage } from "../models/interfaces"
 import { toTalk, toGenerate } from "../functions"
 
 export function useChat() {
 	const [chatId] = useSessionStorage<string>("chatId", toGenerate())
-	const [messages, setMessages] = useSessionStorage<Message[]>("messages", [])
+	const [messages, setMessages] = useSessionStorage<IMessage[]>("messages", [])
 	const [thinking, setThinking] = useState(false)
 
 	const addMessage = async (text: string) => {
 		try {
 			setThinking(true)
-			const newMessage: Message = {
+			const newMessage: IMessage = {
 				id: toGenerate(),
 				text,
 				author: "Visitante",
 				side: "right",
 			}
 			setMessages([...messages, newMessage])
-			const response = await toTalk(chatId, newMessage)
+			const response = await toTalk({chatId: chatId, message: newMessage})
+
 			if (!response) return
-			const responseMessage: Message = {
+			const responseMessage: IMessage = {
 				id: toGenerate(),
 				text,
 				author: "Assistente",
 				side: "left",
 			}
+			
 			setMessages([...messages, responseMessage])
 		} finally {
 			setThinking(false)
