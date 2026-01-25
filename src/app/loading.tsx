a24c301737c219878540730883891429bb073727
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-01-25 14:53:43 -0300

    style: adding icon loader spin

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
-import { LoaderFive } from "@/components/ui/loader";
+
+import { IconLoader } from "@tabler/icons-react";
 
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <LoaderFive text="Carregando..." />
+      <IconLoader className="animate-spin text-red-500" size={48} />
     </div>
   );
 }
